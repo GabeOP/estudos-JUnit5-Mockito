1c0aea98e8385c8001a35ae513918f42fcab2ef4
GabeOP <gabrieloliveirap1@hotmail.com>
2023-07-27 12:53:50 -0300

    adicionado metodo listarTodos

--- src/main/java/com/gabriel/estudosolojunit/controller/ProdutoController.java
+++ src/main/java/com/gabriel/estudosolojunit/controller/ProdutoController.java
@@ -8,12 +8,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
 
   @Autowired
   ProdutoService service;
+
+  @GetMapping
+  public ResponseEntity<List<Produto>> listarTodos() {
+    List<Produto> lista = service.listarTodos();
+    return ResponseEntity.status(HttpStatus.OK).body(lista);
+  }
 
   @PostMapping
   public ResponseEntity<Produto> adicionar(@Valid @RequestBody Produto produto) {
--- src/main/java/com/gabriel/estudosolojunit/service/ProdutoService.java
+++ src/main/java/com/gabriel/estudosolojunit/service/ProdutoService.java
@@ -5,13 +5,18 @@ import com.gabriel.estudosolojunit.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProdutoService {
 
   @Autowired
   ProdutoRepository repository;
 
-  public Produto criar(Produto produto) {
+  public List<Produto> listarTodos() {
+    return repository.findAll();
+  }
+  public Produto adicionar(Produto produto) {
     repository.save(produto);
     return produto;
   }
