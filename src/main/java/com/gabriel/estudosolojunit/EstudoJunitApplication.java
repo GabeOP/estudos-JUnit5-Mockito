f7225cb5bbd22f4c96afc28d4237453289509979
GabeOP <gabrieloliveirap1@hotmail.com>
2023-07-27 11:29:24 -0300

    initial commit

---
+++ src/main/java/com/gabriel/estudosolojunit/EstudoJunitApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.estudosolojunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoJunitApplication.class, args);
+	}
+
+}
