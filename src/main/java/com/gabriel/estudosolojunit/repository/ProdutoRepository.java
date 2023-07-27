81f1454a1de730a91f32507a41f5834299e6ab3a
GabeOP <gabrieloliveirap1@hotmail.com>
2023-07-27 12:40:28 -0300

    criacao classe ProdutoRepository

---
+++ src/main/java/com/gabriel/estudosolojunit/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.estudosolojunit.repository;
+
+import com.gabriel.estudosolojunit.model.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
