e3b35f07b84966449284e7ea7a56c92a83012ac5
GabeOP <gabrieloliveirap1@hotmail.com>
2023-08-04 12:28:44 -0300

    metodo listarPorId

--- src/main/java/com/gabriel/estudosolojunit/service/CategoriaService.java
+++ src/main/java/com/gabriel/estudosolojunit/service/CategoriaService.java
@@ -1,6 +1,8 @@
 package com.gabriel.estudosolojunit.service;
 
 import com.gabriel.estudosolojunit.model.dto.CategoriaDTO;
+import com.gabriel.estudosolojunit.model.entities.Categoria;
+import com.gabriel.estudosolojunit.model.exceptions.NaoEncontradoException;
 import com.gabriel.estudosolojunit.repository.CategoriaRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +23,10 @@ public class CategoriaService {
   public List<CategoriaDTO> listarTodos() {
     return repository.findAll()
             .stream().map(x -> mapper.map(x, CategoriaDTO.class)).collect(Collectors.toList());
+  }
+
+  public CategoriaDTO listarPorId(Long id) {
+    Categoria categoria = repository.findById(id).orElseThrow(() -> new NaoEncontradoException("m"));
+    return mapper.map(categoria, CategoriaDTO.class);
   }
 }
