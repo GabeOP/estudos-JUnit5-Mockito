acfd6afa5b348239932a703eee58652502e0494e
GabeOP <gabrieloliveirap1@hotmail.com>
2023-08-06 13:40:35 -0300

    fix annotation @ExtendWith(SpringExtension.class)

--- src/test/java/com/gabriel/estudosolojunit/service/ProdutoServiceTest.java
+++ src/test/java/com/gabriel/estudosolojunit/service/ProdutoServiceTest.java
@@ -8,11 +8,12 @@ import com.gabriel.estudosolojunit.model.exceptions.NaoEncontradoException;
 import com.gabriel.estudosolojunit.repository.ProdutoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,7 +23,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class ProdutoServiceTest {
 
   public static final String NOME = "Computador";
