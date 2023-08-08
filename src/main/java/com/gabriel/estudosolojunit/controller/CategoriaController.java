d4e012a52f1f5ddcdf5401076b7b6dd99024d88d
GabeOP <gabrieloliveirap1@hotmail.com>
2023-08-08 15:39:28 -0300

    cria classe CategoriaController com metodo listarTodos

---
+++ src/main/java/com/gabriel/estudosolojunit/controller/CategoriaController.java
@@ -0,0 +1,27 @@
+package com.gabriel.estudosolojunit.controller;
+
+import com.gabriel.estudosolojunit.model.dto.CategoriaDTO;
+import com.gabriel.estudosolojunit.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+  @Autowired
+  CategoriaService service;
+
+  @GetMapping
+  public ResponseEntity<List<CategoriaDTO>> listarTodos() {
+    List<CategoriaDTO> lista = service.listarTodos();
+    return ResponseEntity.status(HttpStatus.OK).body(lista);
+  }
+
+}
