2ed71ae52eaf851930b2057fe4378692d1e70909
GabeOP <gabrieloliveirap1@hotmail.com>
2023-08-08 15:58:30 -0300

    metodo listarPorId (CategoriaController)

--- src/main/java/com/gabriel/estudosolojunit/controller/CategoriaController.java
+++ src/main/java/com/gabriel/estudosolojunit/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class CategoriaController {
   public ResponseEntity<List<CategoriaDTO>> listarTodos() {
     List<CategoriaDTO> lista = service.listarTodos();
     return ResponseEntity.status(HttpStatus.OK).body(lista);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CategoriaDTO> listarPorId(@PathVariable Long id) {
+    CategoriaDTO dto = service.listarPorId(id);
+    return ResponseEntity.status(HttpStatus.OK).body(dto);
   }
 
 }
