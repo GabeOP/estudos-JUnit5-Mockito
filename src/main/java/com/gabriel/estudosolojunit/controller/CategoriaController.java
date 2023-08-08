08ccfcfe753f29d2863878ee7ec4720bbcae3877
GabeOP <gabrieloliveirap1@hotmail.com>
2023-08-08 16:42:23 -0300

    metodo adicionar CategoriaController

--- src/main/java/com/gabriel/estudosolojunit/controller/CategoriaController.java
+++ src/main/java/com/gabriel/estudosolojunit/controller/CategoriaController.java
@@ -2,13 +2,11 @@ package com.gabriel.estudosolojunit.controller;
 
 import com.gabriel.estudosolojunit.model.dto.CategoriaDTO;
 import com.gabriel.estudosolojunit.service.CategoriaService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +27,12 @@ public class CategoriaController {
   public ResponseEntity<CategoriaDTO> listarPorId(@PathVariable Long id) {
     CategoriaDTO dto = service.listarPorId(id);
     return ResponseEntity.status(HttpStatus.OK).body(dto);
+  }
+
+  @PostMapping
+  public ResponseEntity<CategoriaDTO> adicionar(@Valid @RequestBody CategoriaDTO obj) {
+    CategoriaDTO dto = service.adicionar(obj);
+    return ResponseEntity.status(HttpStatus.CREATED).body(dto);
   }
 
 }
